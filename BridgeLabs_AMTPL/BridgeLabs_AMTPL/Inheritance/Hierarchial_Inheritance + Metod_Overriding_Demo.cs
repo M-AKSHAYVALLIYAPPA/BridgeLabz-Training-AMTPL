f11ebb155df4becc520844694ee43ceab4b2bf61
M.Akshay <m.akshayvalliyappa@gmail.com>
2025-10-30 13:10:30 +0530

    Created Demo codes to implement different types of Inheritance in feature/OOPS branch under the folder Inheritance

---
+++ BridgeLabs_AMTPL/BridgeLabs_AMTPL/Inheritance/Hierarchial_Inheritance + Metod_Overriding_Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Animal2
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("Animal makes a Sound");
+    }
+}
+
+class Dog2 : Animal2
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog is Barking");
+    }
+}
+
+class Cat2 : Animal2
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Cat is Meowing");
+    }
+}
+
+class Program2
+{
+    static void Main()
+    {
+        Animal2 animal = new Animal2();
+        animal.Sound();
+
+        Dog2 dog = new Dog2();
+        dog.Sound();
+
+        Cat2 cat = new Cat2();
+        cat.Sound();
+    }
+}
