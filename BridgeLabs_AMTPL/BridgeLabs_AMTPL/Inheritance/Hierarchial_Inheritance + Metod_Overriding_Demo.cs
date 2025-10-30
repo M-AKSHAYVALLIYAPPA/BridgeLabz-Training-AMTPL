e6f007ed9259d800443deab935af4ae4cbbbc704
M.Akshay <m.akshayvalliyappa@gmail.com>
2025-10-30 13:16:12 +0530

    Added some New comments on Heirarchial_Inheritance + Overriding Demo.cs file

--- BridgeLabs_AMTPL/BridgeLabs_AMTPL/Inheritance/Hierarchial_Inheritance + Metod_Overriding_Demo.cs
+++ BridgeLabs_AMTPL/BridgeLabs_AMTPL/Inheritance/Hierarchial_Inheritance + Metod_Overriding_Demo.cs
@@ -2,7 +2,7 @@
 
 class Animal2
 {
-    public virtual void Sound()
+    public virtual void Sound() //always use virtual method for overriding
     {
         Console.WriteLine("Animal makes a Sound");
     }
@@ -28,13 +28,13 @@ class Program2
 {
     static void Main()
     {
-        Animal2 animal = new Animal2();
-        animal.Sound();
+        Animal2 animal = new Animal2(); //Animal Object Created
+        animal.Sound(); //Animal Sound method is now called 
 
-        Dog2 dog = new Dog2();
-        dog.Sound();
+        Dog2 dog = new Dog2(); //Dog object is created
+        dog.Sound(); //Overrided Sound Method from Animal called from Dog Class
 
-        Cat2 cat = new Cat2();
-        cat.Sound();
+        Cat2 cat = new Cat2(); //Cat object is created
+        cat.Sound(); //Overrided Sound Method from Animal called from Cat Class
     }
 }
