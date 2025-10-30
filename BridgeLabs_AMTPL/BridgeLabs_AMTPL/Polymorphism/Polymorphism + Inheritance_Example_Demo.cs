f46ff3f21fbe0e305f1bbf7c5fc697bdff74e3d4
M.Akshay <m.akshayvalliyappa@gmail.com>
2025-10-30 13:30:19 +0530

    Started with Implementing Polymorphism

---
+++ BridgeLabs_AMTPL/BridgeLabs_AMTPL/Polymorphism/Polymorphism + Inheritance_Example_Demo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Animal3
+{
+    public virtual void Moves()
+    {
+        Console.WriteLine("Animal is Moving");
+    }
+}
+
+class Bird : Animal3
+{
+    public override void Moves()
+    {
+        Console.WriteLine("Bird is Flying");
+    }
+}
+
+class Fish : Animal3
+{
+    public override void Moves()
+    {
+        Console.WriteLine("Fish is Swimming");
+    }
+}
+
+class Human : Animal3
+{
+    public override void Moves()
+    {
+        Console.WriteLine("Human is Walking/Running");
+    }
+}
+
+class Program3
+{
+    static void Main()
+    {
+        Animal3 MyAnimal = new Animal3();
+        MyAnimal.Moves();
+
+        Animal3 MyBird = new Bird();
+        MyBird.Moves();
+
+        Animal3 MyFish = new Fish();
+        MyFish.Moves();
+
+        Animal3 MyHuman = new Human();
+        MyHuman.Moves();
+    }
+}
